28c297ad0bd429a9fff83f02e7dc30659d992ac6
Aleh Konan <olegkonanff@gmail.com>
2023-02-18 13:06:15 +0400

    picture cell work

--- components/WordsTable/cells/PictureCell.tsx
+++ components/WordsTable/cells/PictureCell.tsx
@@ -1,18 +1,36 @@
 import type { Word } from '@/typings';
+import { Button, message, Upload, UploadFile } from 'antd';
 import Image from 'next/image';
+import { useId, useState } from 'react';
+import styles from './PictureCell.module.css';
 
 type Props = {
   word: Word;
 };
 
 export const PictureCell = ({ word }: Props) => {
+  const id = useId();
+
+  const [file, setFile] = useState<UploadFile>({
+    uid: id,
+    name: word.en,
+    status: 'done',
+    url: word.pictureUrl || undefined,
+  });
+
   return (
-    <Image
-      src={word.pictureUrl || ''}
-      width={100}
-      height={100}
-      alt={word.en}
-      style={{ objectFit: 'cover' }}
-    />
+    <div>
+      <Upload
+        name={word.en}
+        accept="image/*"
+        fileList={[file]}
+        customRequest={({ file }) => console.log('custom request')}
+        onChange={(params) => setFile(params.file)}
+        onDrop={(dropEvent) => console.log('drop ', dropEvent)}
+        onRemove={(file) => console.log('remove ', file)}
+      >
+        <Button>Select file</Button>
+      </Upload>
+    </div>
   );
 };
