ce5250a83d50595083f50c9413779236d9dd9743
Aleh Konan <olegkonanff@gmail.com>
2023-03-03 16:29:39 +0400

    feat: start picture upload feature

--- components/WordsTable/cells/PictureCell.tsx
+++ components/WordsTable/cells/PictureCell.tsx
@@ -1,7 +1,9 @@
 import type { Word } from '@/typings';
-import { Button, message, Upload, UploadFile } from 'antd';
+import { useState } from 'react';
+import { useUploadPicture } from '@/reactQuery/picture';
+import { Button, Space, Upload, UploadFile } from 'antd';
+import AntdImgCrop from 'antd-img-crop';
 import Image from 'next/image';
-import { useId, useState } from 'react';
 import styles from './PictureCell.module.css';
 
 type Props = {
@@ -9,28 +11,59 @@ type Props = {
 };
 
 export const PictureCell = ({ word }: Props) => {
-  const id = useId();
+  const { upload, isUploading } = useUploadPicture(word.id);
+  const [uploadFile, setUploadFile] = useState<UploadFile>();
 
-  const [file, setFile] = useState<UploadFile>({
-    uid: id,
-    name: word.en,
-    status: 'done',
-    url: word.pictureUrl || undefined,
-  });
+  const onUpload = async () => {
+    const formData = new FormData();
+    if (uploadFile instanceof Blob) {
+      formData.append('picture', uploadFile, word.en);
+      upload(formData);
+    }
+  };
+
+  const onCancel = () => {
+    setUploadFile(undefined);
+  };
 
   return (
-    <div>
-      <Upload
-        name={word.en}
-        accept="image/*"
-        fileList={[file]}
-        customRequest={({ file }) => console.log('custom request')}
-        onChange={(params) => setFile(params.file)}
-        onDrop={(dropEvent) => console.log('drop ', dropEvent)}
-        onRemove={(file) => console.log('remove ', file)}
-      >
-        <Button>Select file</Button>
-      </Upload>
-    </div>
+    <Space direction="vertical" align="center">
+      <AntdImgCrop>
+        <Upload
+          accept="image/*"
+          onChange={({ file }) => setUploadFile(file)}
+          beforeUpload={() => false}
+          itemRender={() => null}
+        >
+          {word.pictureUrl ? (
+            <Image
+              className={styles.picture}
+              src={word.pictureUrl}
+              alt={word.en}
+              width={100}
+              height={100}
+              priority
+            />
+          ) : (
+            <Button size="small">Select picture</Button>
+          )}
+        </Upload>
+      </AntdImgCrop>
+      {uploadFile && (
+        <Space>
+          <Button
+            type="primary"
+            size="small"
+            onClick={onUpload}
+            loading={isUploading}
+          >
+            Upload
+          </Button>
+          <Button size="small" onClick={onCancel}>
+            Cancel
+          </Button>
+        </Space>
+      )}
+    </Space>
   );
 };
